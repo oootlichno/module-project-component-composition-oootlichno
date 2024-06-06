00207b10cb04492105f0ca800cce7bdcd0dc407a
your-username <your-email>
2024-06-05 20:06:26 -0500

    Demo data

--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -1,2 +1,4 @@
-export const NASA_URL = `https://api.nasa.gov/planetary/apod`
-export const API_KEY = 'DEMO_KEY'
+
+export const NASA_URL = 'https://api.nasa.gov/planetary/apod';
+export const API_KEY = 'DEMO_KEY';
+
