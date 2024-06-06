8e5b337262c3c6b7bb6e0c63b6de1d7c43182ef8
your-username <your-email>
2024-06-05 19:20:58 -0500

    First block

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,12 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { NASA_URL, API_KEY } from '../constants';
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+  const [currentDataId, setCurrentDataId] = useState(null);
+
+  useEffect(() => {
+    axios.get(`${NASA_URL}?api_key=${API_KEY}`)
+      .then(res => {
+        console.log(res.data);
+        setData(res.data);  
+      })
+      .catch(err => console.log(err));
+  }, []);
+
   return (
-    <p>
-      Read through the instructions in the README.md file to build your NASA
-      app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-    </p>
-  )
+    <div id="root">
+   
+    <div className="content">
+      {data && (
+        <>
+          <img className="photo" src={data.url} alt={data.title} />
+          <div className="text">
+            <h2>{data.title}</h2>
+            <p>{data.explanation}</p>
+            <p className="date">{data.date}</p>
+          </div>
+        </>
+      )}
+    </div>
+  </div>
+);
 }
 
-export default App
---
+++ frontend/constants/index.js
@@ -0,0 +1,2 @@
+export const NASA_URL = 'https://api.nasa.gov/planetary/apod'
+export const API_KEY = 'toYosjfNYSTAomymromawkjulSZlDQd15WV20vza'
