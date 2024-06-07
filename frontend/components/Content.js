6949d988cac2c0e5494bdc9fe9bd389f37ed8f0c
your-username <your-email>
2024-06-07 12:39:21 -0500

    Updated design

--- frontend/components/Content.js
+++ frontend/components/Content.js
@@ -1,15 +1,55 @@
 import React from 'react'
+import styled from "styled-components";
+
+const StyleContent = styled.div`
+
+display: flex;
+align-items: center;
+gap: 100px; 
+padding: 20px;
+
+&:hover {
+    background-color: #93005a;
+    p{
+      color: white;
+    }
+    h2{
+      color: white;
+      font-weight: width; 
+    }
+    date{
+      color: white;
+    }
+  }
+
+h2{
+  color: #93005a;
+  text-align: left;
+  margin-bottom: 20px;
+}
+
+p{
+  font-family: 'Titillium Web', sans-serif;
+  margin-top: 10px;
+  
+}
+
+date {
+  color: #93005a;
+}
+
+`
 
 export default function Content({ title, explanation, url, date }) {
 
     return (
-      <div className="content">
+      <StyleContent className='content'>
         <img className="photo" src={url} alt={title} />
         <div className="text">
           <h2>{title}</h2>
           <p>{explanation}</p>
-          <p className="date">{date}</p>
+          <p><date>DATE: {date}</date></p>
         </div>
-      </div>
+      </StyleContent>
     );
   }
--- frontend/constants/index.js
+++ frontend/constants/index.js
@@ -1,4 +1,4 @@
 
 export const NASA_URL = 'https://api.nasa.gov/planetary/apod';
-export const API_KEY = 'DEMO_KEY';
+export const API_KEY = 'toYosjfNYSTAomymromawkjulSZlDQd15WV20vza';
 
